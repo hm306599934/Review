dafbb22ac7207d178ba4aaba2f55a6cef8813d50
Jimmy <Jimmy@Jimmys-Mac-mini.local>
2018-03-14 08:14:33 +0800

    ok

--- Review/Content/HMTextTableVC.swift
+++ Review/Content/HMTextTableVC.swift
@@ -28,33 +28,20 @@ class HMTextTableVC: UIViewController {
 		mTableView.reloadData()
 	}
 	
-	var middleData: [(String, [(String, String)])] {
-		
-		let basic = ("Swift", [("Swift", ""), ("各版本特性", ""), ("混编", "")])
-		let pattern = ("设计模式", [("设计思想", ""), ("系统设计模式", ""), ("单例模式", ""), ("响应链", ""), ("代理模式", "")])
-		let net = ("网络", [("http", ""), ("https", ""), ("socket", ""), ("抓包", ""), ("AFNetWorking", "")])
-		let framework = ("第三方库", [("下拉刷新", ""), ("SDWebImage", ""), ("AFNetWorking", ""), ("RxSwift原理", ""), ("Weex原理", "")])
-		
-		return [basic, pattern, net, framework]
-	}
-	
-	var highData: [(String, [(String, String)])] {
-		
-		let basic = ("架构", [("架构设计", ""), ("路由方案", ""), ("MGJ路由", ""), ("MVVM", ""), ("RxSwift", "")])
-		let newTech = ("新技术", [("Weex", ""), ("微信小程序", ""), ("自动化测试", ""), ("Python", ""), ("人脸识别", ""), ("AI", "")])
-		let efficiency = ("性能优化", [("性能优化方向", ""), ("内存检测", ""), ("UITableView", ""), ("电量", ""), ("检测工具", "")])
-		let demo = ("应用", [("Runtime空白页", ""), ("Runtime检测UI卡顿", ""), ("自定义KVO", ""), ("自定义消息中心", ""), ("AOP", ""), ("埋点", "")])
-		let other = ("其他", [("自我介绍", ""), ("开发经验", ""), ("发展规划", ""), ("遇到的问题", ""), ("总结", "")])
-		
-		return [basic, newTech, efficiency, demo, other]
-	}
-	
 	
 	lazy var sectionData:[SectionData] = {
 		
 		var result = [SectionData]()
 		
-		let lowDataPath = Bundle.main.path(forResource: "LowData", ofType: "plist") ?? ""
+		var pathName = "LowData"
+		
+		if self.vcTitle == "中级" {
+			pathName = "MiddleData"
+		} else if self.vcTitle == "高级" {
+			pathName = "HighData"
+		}
+		
+		let lowDataPath = Bundle.main.path(forResource: pathName, ofType: "plist") ?? ""
 		
 		if let array = NSArray(contentsOfFile: lowDataPath) {
 			
